aa5c3f92282f030b48646b13f22cf3d7843b4a70
OgnjenPetrovic <zoomght@gmail.com>
2017-08-14 21:25:02 +0200

    Vezba 5 - Midleweares

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -5,7 +5,12 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\User;
 class RegisterController extends Controller
-{
+{   
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
     public function create()
     {
     	return view('register.create');
