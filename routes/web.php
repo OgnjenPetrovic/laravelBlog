1f3c752b8632f2f4844eb36aa391d4390b30ef31
OgnjenPetrovic <zoomght@gmail.com>
2017-08-14 22:03:24 +0200

    Vezba 6 vezivanje usera i posta

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,5 @@ Route::post('/posts/{id}/comment', 'CommentsController@store');
 Route::get('/register', 'RegisterController@create');
 Route::post('/register', 'RegisterController@store');
 Route::get('/logout','LoginController@destroy');
-Route::get('/login','LoginController@create');
+Route::get('/login','LoginController@create')->name('login');
 Route::post('/login', 'LoginController@store');
